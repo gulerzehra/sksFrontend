9dabd72b75f04fe2cf243c7822402b7e9b81e269
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-05-15 17:08:10 +0300

    refactor: Update file paths and component names for Blog page

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Event from './pages/Events/Event/EventComp';
 import Register from './pages/Register/RegisterComp';
 import Login from './pages/Login/LoginComp';
 import ClubPage from './pages/Clubs/Club/ClubComp';
-import BlogComp from './pages/Manager/Posts/Add/ManagerAddPostComp';
+import ManagerAddComp from './pages/Manager/Posts/Add/ManagerAddPostComp';
 import Manager from './pages/Manager/ManagerComp';
 import ManagerEvents from './pages/Manager/Events/ManagerEventsComp';
 import ManagerAddEvent from './pages/Manager/Events/Add/ManagerAddEventComp';
@@ -38,7 +38,7 @@ const router = createBrowserRouter([
       { path: 'manage/events', element: <ManagerEvents /> },
       { path: 'manage/events/add', element: <ManagerAddEvent /> },
       { path: 'manage/posts', element: <ManagerPosts /> },
-      { path: 'manage/posts/add', element: <BlogComp /> },
+      { path: 'manage/posts/add', element: <ManagerAddComp /> },
       { path: 'manage/notifications', element: <ManagerNotifications /> },
       // Routes which will be added should be added here, not below of NotFound route
       { path: '*', element: <h1>Not found</h1> },
