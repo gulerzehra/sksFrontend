5ad8460a4b029e20359ef17daf43748556ce131b
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-05-16 23:40:19 +0300

    refactor: Add club, event, and post slices to store configuration

--- src/data/store.js
+++ src/data/store.js
@@ -1,10 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
 import notificationReducer from './notificationSlice';
+import clubsReducer from './clubSlice';
+import eventsReducer from './eventSlice';
+import postReducer from './postSlice';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     notification: notificationReducer,
+    club: clubsReducer,
+    event: eventsReducer,
+    post: postReducer,
   },
 });
