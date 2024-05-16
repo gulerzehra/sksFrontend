100c1af7503931322a1f4f5aefac14769f1144ad
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-05-16 23:41:28 +0300

    refactor: Add lazy loading for club images in HomeComp

--- src/pages/Home/HomeComp.jsx
+++ src/pages/Home/HomeComp.jsx
@@ -2,31 +2,9 @@ import Hero from './components/Hero/HeroComp';
 import Clubs from './components/Clubs/ClubsComp';
 import Recents from './components/Recents/RecentsComp';
 import { useDocumentTitle } from '../../hooks/useDocumentTitle';
-import { useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
 
 function HomeComp() {
   useDocumentTitle('Home');
-  const navigate = useNavigate();
-  const { isLoggedIn } = useSelector((state) => state.user);
-
-  useEffect(() => {
-    if (!isLoggedIn) {
-      navigate('/login');
-    }
-  }, [isLoggedIn, navigate]);
-
-  if (!isLoggedIn) {
-    return (
-      <p>
-        You must be logged in to view this page. Please{' '}
-        <Link to="/login" style={{ color: 'blue' }}>
-          click here to login
-        </Link>
-      </p>
-    );
-  }
 
   return (
     <>
